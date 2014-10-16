113ea2c398a171d877e69208e5dbd15fe7525ef2
BoomYan <jiamingyan@hotmail.com>
2014-10-15 21:39:18 -0400

    Lesson 1 Update

---
+++ js/resumeBuilder.js
@@ -0,0 +1,51 @@
+
+
+////////////Try JavaScript String Method
+// $("#main").append("Jiaming YAN");
+// var awesomeThoughts = "I am Jiaming and I am AWESOME";
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+// var name = "Jiaming Yan";
+// var formattedName = HTMLheaderName.replace("%data%",name);
+// var role = "Web Developer";
+// var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+/////////////Try Using JQuery Selector
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedRole);
+
+
+/////////////Try Arrays
+// var skills = ["awesomeness","programming","teaching"];
+
+
+/////////Try JavaScript Objects
+// var bio = {"name":"James", "role":"Web Developer", "age":32, "skills":["awesomeness","programming","teaching"]};
+// $("#main").append(bio.name);
+// $("#main").append(bio.role);
+// var work={"position":"developer"};
+// work.yearsWorked=9;
+// var edu={"lastSchoolName":"Beihang University"};
+// edu.yearsAttended=2010;
+// $("#main").append(work.position);
+// $("#main").append(edu.lastSchoolName);
+
+
+///////////Try JSON(JavaScript Object Notation)  & Validate it with jsonlint.com
+var edu={
+    "schools": [
+        {
+            "name": "Tieyi middle school",
+            "GraduateYear": "2010"
+        },
+        {
+            "name": "Beihang University",
+            "GraduateYear": "2014"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "name": "JavaScript Basics"
+        }
+    ]
+}
